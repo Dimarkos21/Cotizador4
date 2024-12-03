123a81c908353a10cca583f3ce2a96be47fab877
Dimarkos <104870419+Dimarkos21@users.noreply.github.com>
2024-12-03 18:00:43 -0400

    Add files via upload

---
+++ d.js
@@ -0,0 +1,51 @@
+const numero = document.querySelector("#input")
+const boton =  document.querySelector("#boton")
+const conteo = document.querySelector("#conteo")
+boton.addEventListener("click" , e => {
+  var h = numero.value
+  const intervalo = setInterval(() => {
+ 
+    h = h - 1
+    conteo.innerHTML = `<p> ${h} <p>`
+    
+  console.log(h)
+  
+  
+  
+   
+  
+  if (h === 0){
+    clearInterval(intervalo)
+    console.log("Despegue")
+ conteo.innerHTML = `
+ 
+ 
+ <video id="myVideo" width="640" height="360"  autoplay> <source src="./video.mp4" type="video/mp4"> Tu navegador no soporta la etiqueta de video.`
+  
+  }
+  
+  
+     
+    }, 100);
+
+    
+  
+})
+//setInterval(() => {
+   // console.log("hola");
+   // setInterval(()=> {
+  //  consola.log('que lo que')
+   // })
+  //}, 100);
+
+ 
+  //for (let i = 0; i < 100; i++) { console.log(i);
+
+   // if(i=== 4){
+ //console.log("Despegue")
+   //}
+  //}
+ 
+ 
+
+  
